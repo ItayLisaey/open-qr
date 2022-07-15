bad8f370413c52c3625de636f1a097c353df3ab5
ItayLisaey <ilisaey@gmail.com>
2022-07-15 21:49:21 +0300

    chore: changed favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,16 @@ const HomePage: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'
+          integrity='sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=='
+          crossOrigin='anonymous'
+          referrerPolicy='no-referrer'
+        />
         <title>Barcode Cam</title>
         <meta name='description' content='barcode scanner' />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/scan.svg' />
       </Head>
 
       <Home />
