e01d2aceba5875d5e1dc68d5fb8412f2123fa998
ItayLisaey <ilisaey@gmail.com>
2022-07-15 22:52:29 +0300

    feat: added basic manifest for pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
-}
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+})
 
 module.exports = nextConfig
