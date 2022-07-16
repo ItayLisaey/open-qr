0bbc866df6b8478042a820a472f10d3a59e67673
ItayLisaey <ilisaey@gmail.com>
2022-07-16 10:30:41 +0300

    style: add simple animations

--- components/CameraScan/CameraScan.tsx
+++ components/CameraScan/CameraScan.tsx
@@ -1,9 +1,11 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { motion } from 'framer-motion';
 import QrScanner from 'qr-scanner';
 import { useRef } from 'react';
 import { useVideoScanner } from '../../hooks/useVideoScanner';
+import { AnimationPresets } from '../../styles/animation.config';
 import classes from './camera-scan.module.scss';
 
 export interface CameraScanProps {
@@ -39,9 +41,13 @@ export const CameraScan: React.FC<CameraScanProps> = ({
       <h1>Searching...</h1>
       <video ref={videoRef} />
       <section role='button-container'>
-        <button role='back' onClick={onBack}>
+        <motion.button
+          role='back'
+          onClick={onBack}
+          {...AnimationPresets.standard}
+        >
           <FontAwesomeIcon icon={faTimes as IconProp} />
-        </button>
+        </motion.button>
       </section>
     </div>
   );
